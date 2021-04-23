41961de90f25e705cd605449e5a6509f4674c4c1
MANABE Kazuya <55941572+KazuyaManabe@users.noreply.github.com>
2021-04-23 09:36:02 +0900

    NewApplication

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Your_age your_age = "23" times = "12"/>
     </View>
-    </NavigationContainer>
+  );
+}
+
+function Your_age(props) {
+  return (
+    <Text>You are {props.your_age} years old.
+    I'm {props.your_age * props.times} years old.</Text>
   );
 }
 
--- README.md
+++
@@ -1,2 +0,0 @@
-# Learning-React-Native
-#『基礎から学ぶReact Native入門』を読んで実行したコード
